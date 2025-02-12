125160b49146c7d624f1c55babc1962759fb87c5
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2025-02-12 18:00:53 -0300

    aula 06 - Encapsulamento

---
+++ Aula06/src/aula06.java
@@ -0,0 +1,10 @@
+public class aula06 {
+    public static void main(String[] args) throws Exception {
+        ControleRemoto c = new ControleRemoto();
+
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+    }
+}
